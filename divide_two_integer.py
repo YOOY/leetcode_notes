b3fcb16011ecac3c2db23e1ec4125349db2b3ac6
yooy <yooyhung@gmail.com>
2021-04-24 16:23:54 +0800

    add code

---
+++ divide_two_integer.py
@@ -0,0 +1,27 @@
+# 
+
+def divide(dividend, divisor):
+    maximum = 2**31 - 1
+    minimum = -2**31
+
+    dividend = abs(dividend)
+    divisor = abs(divisor)
+    count = 0
+    
+    while dividend >= divisor:
+        sum = divisor
+        rep = 1
+        while dividend > sum + sum:
+            sum += sum
+            rep += rep
+        dividend -= sum
+        count += rep
+    if (dividend < 0 and divisor > 0) or (dividend > 0 and divisor < 0):
+        count = 0 - count
+    if count < minimum:
+        count = minimum
+    if count > maximum:
+        count = maximum
+    return count
+
+print(divide(10,3))
