273a55587b990f78b47da5122ab717e6f5f63bc5
yooy <yooyhung@gmail.com>
2021-04-25 17:59:29 +0800

    adjust code

--- problem/permutations.py
+++ problem/permutations.py
@@ -16,4 +16,18 @@ def backtracking(nums, container, limit, result):
             backtracking(nums, container, limit, result)
             container.remove(i)
 
+def permute_dfs(nums):
+    res = []
+    dfs(nums, [], res)
+    return res
+    
+def dfs(nums, path, res):
+    if not nums:
+        res.append(path)
+        # return # backtracking
+    for i in range(len(nums)):
+        print(f"i is {i} / nums is {nums} / path = {path}")
+        dfs(nums[:i]+nums[i+1:], path+[nums[i]], res)
+
 print(permute([1,2,3]))
+print(permute_dfs([1,2,3]))
