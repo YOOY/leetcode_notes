e0e3bce4f9ed13044dbab55f86bf88ee70a580c6
yooy <yooyhung@gmail.com>
2021-04-27 07:09:58 +0800

    add code

---
+++ problem/merge_intervals.py
@@ -0,0 +1,11 @@
+def merge(intervals):
+    intervals = sorted(intervals)
+    result = []
+    for i in intervals:
+        if not result or i[0] > result[-1][1]:
+            result.append(i)
+        else:
+            result[-1] = [min(result[-1][0], i[0]), max(result[-1][1], i[1])]
+    return result
+
+print(merge([[1,4],[0,0]]))
