c943ddfaa416c42e25e92551b7267e80160a71bd
diego-hung <diego_hung@trendmicro.com>
2021-05-03 14:57:12 +0800

    adjust code

--- problem/remove_nth_node_from_end_of_list.py
+++ problem/remove_nth_node_from_end_of_list.py
@@ -17,10 +17,26 @@ def removeNthFromEnd(head, n):
     result = stack[0] if stack else None
     return result
 
+def removeNthFromEnd_two_pointer(head, n):
+    fast_node = head
+    slow_node = head
+    for _ in range(n+1):
+        fast_node = fast_node.next
+    while fast_node:
+        fast_node = fast_node.next
+        slow_node = slow_node.next
+    slow_node.next = slow_node.next.next
+    return head
+
 head = ListNode(1)
 head.next = ListNode(2)
 head.next.next = ListNode(3)
 head.next.next.next = ListNode(4)
 head.next.next.next.next = ListNode(5)
 
-print(removeNthFromEnd(head, 2))
+#print(removeNthFromEnd(head, 2))
+print(removeNthFromEnd_two_pointer(head, 2))
+
+while head:
+    print(head.val)
+    head = head.next
