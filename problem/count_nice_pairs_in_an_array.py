8fe3c7497d63b23efcc09e2fc6f1a39b2a587cea
yooy <yooyhung@gmail.com>
2021-05-04 18:06:55 +0800

    adjust code

--- problem/count_nice_pairs_in_an_array.py
+++ problem/count_nice_pairs_in_an_array.py
@@ -1,12 +1,16 @@
-from collections import Counter
+from collections import defaultdict
 def countNicePairs(nums):
-    result = Counter()
+    result = defaultdict(int)
     ans = 0
     for i in nums:
         tmp = i - rev(i)
-        ans += result[tmp]
         result[tmp] += 1
+    
+    for i in result.values():
+        ans += i * (i-1) // 2
     return ans % (10**9 + 7)
 
 def rev(num):
     return int(str(num)[::-1])
+
+print(countNicePairs([13,10,35,24,76]))
