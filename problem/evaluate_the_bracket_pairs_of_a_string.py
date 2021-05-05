3b995f66b35a1dec561c85ec49fea44c3ba92106
yooy <yooyhung@gmail.com>
2021-05-05 09:36:46 +0800

    add code

---
+++ problem/evaluate_the_bracket_pairs_of_a_string.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+def constant_factory():
+    return lambda: '?'
+def evaluate(s, knowledge):
+    key = ""
+    result = ""
+    keyword = False
+    knowledges = defaultdict(constant_factory())
+    for i in knowledge:
+        knowledges[i[0]] = i[1]
+    
+    for i in s:
+        if i == '(':
+            keyword = True
+        elif i == ')':
+            keyword = False
+            result += knowledges[key]
+            key = ""
+        elif keyword:
+            key += i
+        else:
+            result += i
+    return result
+
+print(evaluate("(name)is(age)yearsold", [["name","bob"],["age","two"]]))
