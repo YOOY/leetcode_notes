23e4ad9f4c91c7d42deea801793347484230975d
yooy <yooyhung@gmail.com>
2021-05-30 15:55:26 +0800

    add code

---
+++ problem/largest_merge_of_two_strings.py
@@ -0,0 +1,14 @@
+def largestMerge(word1, word2):
+    ans = ""
+    while word1 and word2:
+        if word1 > word2:
+            ans += word1[0]
+            word1 = word1[1:]
+        else:
+            ans += word2[0]
+            word2 = word2[1:]
+    ans += word1
+    ans += word2
+    return ans
+
+print(largestMerge('abcabc', 'abdcaba'))
